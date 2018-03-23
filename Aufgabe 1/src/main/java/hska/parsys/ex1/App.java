a146346385cb2266d11f792b18e8dede602cc877
Tobias Kerst <tobias.kerst@tobscore.com>
2018-03-23 19:44:46 +0100

    Adds basic project files

---
+++ Aufgabe 1/src/main/java/hska/parsys/ex1/App.java
@@ -0,0 +1,8 @@
+package hska.parsys.ex1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Starting Supermarket Simulation");
+        // TODO: Implement actual program
+    }
+}
