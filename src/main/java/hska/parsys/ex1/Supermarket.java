c93f9d69c36133009f6bb343eb63c522fe694427
Tobias Kerst <tobias.kerst@tobscore.com>
2018-04-10 13:00:01 +0200

    Uses locks instead of synchronized

--- src/main/java/hska/parsys/ex1/Supermarket.java
+++ src/main/java/hska/parsys/ex1/Supermarket.java
@@ -3,9 +3,14 @@ package hska.parsys.ex1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Supermarket {
   private static Logger logger = LoggerFactory.getLogger(Supermarket.class);
   private int freeMachines = 3;
+  private final ReentrantLock lock = new ReentrantLock();
+  private final Condition condition = lock.newCondition();
 
   /**
    * Initializes the supermarket with the given number of reverse vending machines.
@@ -21,26 +26,36 @@ public class Supermarket {
     freeMachines = initialFreeMachines;
   }
 
-  public synchronized void customerEnters(Customer customer) {
+  public void customerEnters(Customer customer) {
+    lock.lock();
     logger.info(
         "Customer #{} ({} bags) enters the supermarket", customer.getNumber(), customer.getBags());
 
-    while(freeMachines == 0) {
-      try {
-        logger.info("Customer #{} has to wait", customer.getNumber());
-        wait();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+    try {
+      while (freeMachines == 0) {
+        try {
+          condition.await();
+          logger.info("Customer #{} has to wait", customer.getNumber());
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
       }
+      freeMachines--;
+      Thread customerThread = new Thread(customer);
+      customer.setSupermarket(this);
+      customerThread.start();
+    } finally {
+      lock.unlock();
     }
-    freeMachines--;
-    Thread customerThread = new Thread(customer);
-    customer.setSupermarket(this);
-    customerThread.start();
   }
 
-  public synchronized void customerLeaves() {
-    freeMachines++;
-    notify();
+  public void customerLeaves() {
+    lock.lock();
+    try {
+      freeMachines++;
+      condition.signal();
+    } finally {
+      lock.unlock();
+    }
   }
 }
